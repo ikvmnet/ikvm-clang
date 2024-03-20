ee1bae1376931a0f759034ee50245bd31b14479c
Jerome Haltom <jhaltom@alethic.solutions>
2024-03-19 21:25:04 -0500

    Fix up test run to output to right places. Use Itemgroup for PreprocessorDefinitions. With optional Value metadata.

--- src/IKVM.Clang.Sdk.Tests/ProjectTests.cs
+++ src/IKVM.Clang.Sdk.Tests/ProjectTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Xml.Linq;
 
 using Buildalyzer;
@@ -46,22 +48,50 @@ namespace IKVM.Clang.Sdk.Tests
 
         public TestContext TestContext { get; set; }
 
-        [TestMethod]
-        public void CanBuildTestProject()
+        public static Dictionary<string, string> Properties { get; set; }
+
+        public static string TestRoot { get; set; }
+
+        public static string TempRoot { get; set; }
+
+        public static string WorkRoot { get; set; }
+
+        public static string NuGetPackageRoot { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
         {
-            var properties = File.ReadAllLines("IKVM.Clang.Sdk.Tests.properties").Select(i => i.Split('=', 2)).ToDictionary(i => i[0], i => i[1]);
+            // properties to load into test build
+            Properties = File.ReadAllLines("IKVM.Clang.Sdk.Tests.properties").Select(i => i.Split('=', 2)).ToDictionary(i => i[0], i => i[1]);
 
-            var nugetPackageRoot = Path.Combine(Path.GetTempPath(), "IKVM.Clang.Sdk.Tests", "nuget", "packages");
-            if (Directory.Exists(nugetPackageRoot))
-                Directory.Delete(nugetPackageRoot, true);
-            Directory.CreateDirectory(nugetPackageRoot);
+            // root of the project collection itself
+            TestRoot = Path.Combine(Path.GetDirectoryName(typeof(ProjectTests).Assembly.Location), "Project");
+            context.WriteLine("TestRoot: {0}", TestRoot);
 
+            // temporary directory
+            TempRoot = Path.Combine(Path.GetTempPath(), "IKVM.Clang.Sdk.Tests", Guid.NewGuid().ToString());
+            if (Directory.Exists(TempRoot))
+                Directory.Delete(TempRoot, true);
+            Directory.CreateDirectory(TempRoot);
+            context.WriteLine("TempRoot: {0}", TempRoot);
+
+            // work directory
+            WorkRoot = Path.Combine(context.TestRunDirectory, "IKVM.Clang.Sdk.Tests", "ProjectTests");
+            if (Directory.Exists(WorkRoot))
+                Directory.Delete(WorkRoot, true);
+            Directory.CreateDirectory(WorkRoot);
+            context.WriteLine("WorkRoot: {0}", WorkRoot);
+
+            // other required sub directories
+            NuGetPackageRoot = Path.Combine(TempRoot, "nuget", "packages");
+
+            // nuget.config file that defines package sources
             new XDocument(
                 new XElement("configuration",
                     new XElement("config",
                         new XElement("add",
                             new XAttribute("key", "globalPackagesFolder"),
-                            new XAttribute("value", nugetPackageRoot))),
+                            new XAttribute("value", NuGetPackageRoot))),
                     new XElement("packageSources",
                         new XElement("clear"),
                         new XElement("add",
@@ -69,21 +99,55 @@ namespace IKVM.Clang.Sdk.Tests
                             new XAttribute("value", "https://api.nuget.org/v3/index.json")),
                         new XElement("add",
                             new XAttribute("key", "dev"),
-                            new XAttribute("value", Path.Combine(Path.GetDirectoryName(typeof(ProjectTests).Assembly.Location), @"nuget"))),
-                        new XElement("add",
-                            new XAttribute("key", "nuget.org"),
-                            new XAttribute("value", "https://api.nuget.org/v3/index.json")))))
-                .Save(Path.Combine(@"Project", "nuget.config"));
+                            new XAttribute("value", Path.Combine(Path.GetDirectoryName(typeof(ProjectTests).Assembly.Location), @"nuget"))))))
+                .Save(Path.Combine(TestRoot, "nuget.config"));
 
             var manager = new AnalyzerManager();
-            var analyzer = manager.GetProject(Path.Combine(@"Project", "Executable", "Executable.clangproj"));
-            analyzer.AddBuildLogger(new TargetLogger(TestContext) { Verbosity = LoggerVerbosity.Detailed });
-            analyzer.AddBinaryLogger("msbuild.binlog");
-
+            var analyzer = manager.GetProject(Path.Combine(TestRoot, "Executable", "Executable.clangproj"));
+            analyzer.AddBuildLogger(new TargetLogger(context));
+            analyzer.AddBinaryLogger(Path.Combine(WorkRoot, "msbuild.binlog"));
             analyzer.SetGlobalProperty("ImportDirectoryBuildProps", "false");
             analyzer.SetGlobalProperty("ImportDirectoryBuildTargets", "false");
-            analyzer.SetGlobalProperty("PackageVersion", properties["PackageVersion"]);
-            analyzer.SetGlobalProperty("RestorePackagesPath", nugetPackageRoot + Path.DirectorySeparatorChar);
+            analyzer.SetGlobalProperty("PackageVersion", Properties["PackageVersion"]);
+            analyzer.SetGlobalProperty("RestorePackagesPath", NuGetPackageRoot + Path.DirectorySeparatorChar);
+            analyzer.SetGlobalProperty("CreateHardLinksForAdditionalFilesIfPossible", "true");
+            analyzer.SetGlobalProperty("CreateHardLinksForCopyAdditionalFilesIfPossible", "true");
+            analyzer.SetGlobalProperty("CreateHardLinksForCopyFilesToOutputDirectoryIfPossible", "true");
+            analyzer.SetGlobalProperty("CreateHardLinksForCopyLocalIfPossible", "true");
+            analyzer.SetGlobalProperty("CreateHardLinksForPublishFilesIfPossible", "true");
+            analyzer.SetGlobalProperty("Configuration", "Release");
+
+            var options = new EnvironmentOptions();
+            options.WorkingDirectory = TestRoot;
+            options.DesignTime = false;
+            options.Restore = false;
+            options.TargetsToBuild.Clear();
+            options.TargetsToBuild.Add("Clean");
+            options.TargetsToBuild.Add("Restore");
+            options.Arguments.Add("/v:d");
+            analyzer.Build(options).OverallSuccess.Should().Be(true);
+
+            context.AddResultFile(Path.Combine(WorkRoot, "msbuild.binlog"));
+        }
+
+        [DataTestMethod]
+        [DataRow(EnvironmentPreference.Core, "x86_64-pc-windows-msvc")]
+        [DataRow(EnvironmentPreference.Core, "i686-pc-windows-msvc")]
+        [DataRow(EnvironmentPreference.Core, "aarch64-pc-windows-msvc")]
+        public void CanBuildTestProject(EnvironmentPreference env, string tid)
+        {
+            TestContext.WriteLine("TestRoot: {0}", TestRoot);
+            TestContext.WriteLine("TempRoot: {0}", TempRoot);
+            TestContext.WriteLine("WorkRoot: {0}", WorkRoot);
+
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(Path.Combine(TestRoot, "Executable", "Executable.clangproj"));
+            analyzer.AddBuildLogger(new TargetLogger(TestContext));
+            analyzer.AddBinaryLogger(Path.Combine(WorkRoot, $"{tid}-msbuild.binlog"));
+            analyzer.SetGlobalProperty("ImportDirectoryBuildProps", "false");
+            analyzer.SetGlobalProperty("ImportDirectoryBuildTargets", "false");
+            analyzer.SetGlobalProperty("PackageVersion", Properties["PackageVersion"]);
+            analyzer.SetGlobalProperty("RestorePackagesPath", NuGetPackageRoot + Path.DirectorySeparatorChar);
             analyzer.SetGlobalProperty("CreateHardLinksForAdditionalFilesIfPossible", "true");
             analyzer.SetGlobalProperty("CreateHardLinksForCopyAdditionalFilesIfPossible", "true");
             analyzer.SetGlobalProperty("CreateHardLinksForCopyFilesToOutputDirectoryIfPossible", "true");
@@ -92,39 +156,19 @@ namespace IKVM.Clang.Sdk.Tests
             analyzer.SetGlobalProperty("SkipCopyBuildProduct", "false");
             analyzer.SetGlobalProperty("CopyBuildOutputToOutputDirectory", "true");
 
+            var options = new EnvironmentOptions();
+            options.WorkingDirectory = TestRoot;
+            options.Preference = env;
+            options.DesignTime = false;
+            options.Restore = false;
+            options.GlobalProperties["TargetIdentifier"] = tid;
+            options.TargetsToBuild.Clear();
+            options.TargetsToBuild.Add("Clean");
+            options.TargetsToBuild.Add("Build");
+            options.Arguments.Add("/v:d");
+            analyzer.Build(options).OverallSuccess.Should().BeTrue();
 
-            var o = new EnvironmentOptions();
-
-            // first restore and clean
-            o.TargetsToBuild.Clear();
-            o.TargetsToBuild.Add("Restore");
-            o.TargetsToBuild.Add("Clean");
-            analyzer.Build(o).OverallSuccess.Should().BeTrue();
-
-            // do a global build
-            o.TargetsToBuild.Clear();
-            o.TargetsToBuild.Add("Build");
-            analyzer.Build(o).OverallSuccess.Should().BeTrue();
-
-            // do individual target builds
-            var targets = new[]
-            {
-                "x86_64-pc-windows-msvc",
-                "i686-pc-windows-msvc",
-                "aarch64-pc-windows-msvc",
-            };
-
-            foreach (var target in targets)
-            {
-                TestContext.WriteLine("Building with TargetIdentifier {0}.", target);
-                var options = new EnvironmentOptions();
-                options.DesignTime = false;
-                options.Restore = false;
-                options.GlobalProperties["TargetIdentifier"] = target;
-                options.TargetsToBuild.Clear();
-                options.TargetsToBuild.Add("Build");
-                analyzer.Build(o).OverallSuccess.Should().Be(true);
-            }
+            TestContext.AddResultFile(Path.Combine(WorkRoot, $"{tid}-msbuild.binlog"));
         }
 
     }
