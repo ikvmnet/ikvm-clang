b25efaf748c4c47c76518cece5566832bf54683e
Jerome Haltom <jhaltom@alethic.solutions>
2026-06-03 13:02:42 -0500

    Define Base paths outside currentversion.

--- src/IKVM.Clang.Vsix/Packaging/ClangPackage.cs
+++ src/IKVM.Clang.Vsix/Packaging/ClangPackage.cs
@@ -1,9 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using IKVM.Clang.Vsix.Registration;
 
-using IKVM.Clang.Vsix.Registration;
-
-using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+
+using System.Runtime.InteropServices;
 
 namespace IKVM.Clang.Vsix.Packaging
 {
@@ -12,8 +11,6 @@ namespace IKVM.Clang.Vsix.Packaging
     [Guid(PackageGuid)]
     [InstalledProductRegistration("#110", "#112", "1.0")]
     [ProvideBindingPath]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideTextMateGrammars("Grammars")]
     [ProvideImageManifest("IKVM.Clang.imagemanifest")]
     public sealed class ClangPackage : AsyncPackage
